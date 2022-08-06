a4588c2481f9408a93275393792da0d015b6e293
manijain25 <manijainbdh@gmail.com>
2022-08-06 19:15:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import re
 
-
 def func1(query):
+    res = re.findall(r'\(.*?\)', query)
+    word = "FROM"
+    for t in res:
+        if word in t:
+            x = re.split('FROM | WHERE ', t)
+            tbl = x[1]
+            # print (t)
+            t1 = "{}".format(t)
+            query = query.replace(t1, tbl)
+
+    return query
+def func2(query1):
     # Use a breakpoint in the code line below to debug your script.
+    query = func1(query1)
+    query = re.sub(r"[\n\t\(\)]*", "", query)
     result = re.split('SELECT |FROM | WHERE ', query)
     lst1 = []
     lst2 = []
@@ -10,6 +23,7 @@ def func1(query):
     for s in result[2].split(", "):
         # print(s)
         for T in reversed(s.split(" ")):
+            T = T.strip()
             lst1.append(T)
             tpl = lst1
         lst1 = []
@@ -26,6 +40,7 @@ def func1(query):
         # print(s)
         r2 = re.split("\.", s)
         for T in reversed(r2):
+            T = T.strip()
             lst1.append(T)
             tpl = lst1
         lst1 = []
@@ -45,7 +60,8 @@ def func1(query):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    query = "LOCKING ROW FOR ACCESS SELECT T.SWB_CNTRY_ID,T.CNTRY_TYPE_CD,T.DW_EFF_DT,S.DW_AS_OF_DT FROM IDW_DATA.CNTRY_MULTI_DEF_CD_T T, IDW_STAGE.CNTRY_MULTI_DEF_CD_S S WHERE S.SWB_CNTRY_ID = T.SWB_CNTRY_ID AND S.CNTRY_SCHEME_CD = T.CNTRY_TYPE_CD "
-    func1(query)
+    query = "LOCKING ROW FOR ACCESS SELECT T.SWB_CNTRY_ID, T.CNTRY_TYPE_CD, T.DW_EFF_DT, S.DW_AS_OF_DT FROM (SELECT SWB_CNTRY_ID, CNTRY_TYPE_CD, RCV_IN, DW_EFF_DT, MAXDW_EFF_DT MAX_EFF_DT FROM IDW_DATA.CNTRY_MULTI_DEF_CD_T WHERE CURR_IN=1 GROUP BYa 1,2,3,4) T, (SELECT SWB_CNTRY_ID, CNTRY_SCHEME_CD, DW_AS_OF_DT, DW_ACTN_IND FROM IDW_STAGE.CNTRY_MULTI_DEF_CD_S) S WHERE S.SWB_CNTRY_ID = T.SWB_CNTRY_ID AND S.CNTRY_SCHEME_CD = T.CNTRY_TYPE_CD AND (S.DW_SCTN_IND=U OR (S.DW_ACTN_IND=I AND T.RCV_IN=0)) AND S.DW_AS_OF_DT > T.MAX_EFF_DT "
+
+    func2(query)
 
 
